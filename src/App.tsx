d429943bf6704db471c2d38780501af6abfc9455
Alexanderu9 <alexanderu7.aug@gmail.com>
2025-05-25 19:00:12 -0300

    Ex 5 – Loja de plantas: Tema
    Você deve criar uma variação do tema claro e escuro utilizando o theme do Material UI. O tema claro deve ser baseado em um tom de marrom e, o escuro, em um tom de verde; devemos ter um botão para fazer a mudança destes temas.
    
    Deve aplicar o temas no default e no paper; nesse caso, aqui, é interessante a utilização do paper em pontos chaves do projeto para colaborar com o design.
    
    Sugestão de cores:
    Claro: #c2a39a | #e1d1c8
    Escuro: #001600 | #0a3007

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,84 @@
-import './App.css'
-import PlantCard from './components/molecules/molecules'
+import { Button, Paper } from '@mui/material';
+import './App.css';
+import PlantCard from './components/molecules/molecules';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState } from 'react';
 
 function App() {
 
+  const claroTheme = createTheme({
+    palette: {
+      mode: 'light',
+      background: {
+        default: '#e1d1c8',
+        paper: '#c2a39a',
+      },
+      primary: {
+        main: '#c2a39a',
+      },
+      secondary: {
+        main: '#6d4c41', 
+      },
+    },
+  });
+
+
+  const escuroTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      background: {
+        default: '#0a3007',
+        paper: '#001600',
+      },
+      primary: {
+        main: '#0a3007',
+      },
+      secondary: {
+        main: '#4caf50',
+      },
+    },
+  });
+
+  const [isEscureTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkTheme((prev) => !prev);
+  };
+
+  const atualTheme = isEscureTheme ? escuroTheme : claroTheme;
 
   return (
-    <>
-     <PlantCard buttonText='Comprar Planta' 
-    name='Planta' 
-    price={12.32} buttonVariant='secundary'
-    iconSize='lg' 
-    sx={{color: 'green'}}
-    id={1}
-    light='sun'/>
-    
-    </>
-  )
+    <ThemeProvider theme={atualTheme}>
+      <Paper
+        style={{
+          minHeight: '100vh',
+          padding: '2rem',
+          backgroundColor: atualTheme.palette.background.default,
+        }}
+        elevation={0}
+      >
+        <PlantCard
+          buttonText="Comprar Planta"
+          name="Planta"
+          price={12.32}
+          buttonVariant='primary'
+          iconSize="lg"
+          sx={{ color: 'green' }}
+          id={1}
+          light="shade"
+        />
+        <Button
+          variant="contained"
+          onClick={toggleTheme}
+          color="secondary"
+          sx={{ marginTop: '1rem' }}
+        >
+          Mudar Tema
+        </Button>
+      </Paper>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
+
